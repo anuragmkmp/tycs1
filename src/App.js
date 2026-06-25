fa0853055175a3640d1dc7ae09efd7a41159732e
anuragsir <mkmpanurag@gmail.com>
2026-06-25 12:56:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello TYCS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mkmpatel.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MKMP
         </a>
       </header>
     </div>
