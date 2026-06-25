76357d221a42a9929f6ab2db22410e7fe9a52082
anuragsir <mkmpanurag@gmail.com>
2026-06-25 13:09:45 +0530

    Changes

---
+++ src/Tycs.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function Tycs() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello TYCS Students
+        </p>
+
+        <p>
+            A . Y. 2026-27
+        </p>
+        <a
+          className="App-link"
+          href="https://mkmpatel.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MKMP
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Tycs;
